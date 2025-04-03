687dac4172d19980b79d20b715c910b7f215ea5e
nikita-solovyov-dev <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-04-03 18:52:17 +0300

    Added 'created user table' migration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-litestar database upgrade
-uvicorn app.main:app --reload --host 0.0.0.0  --port 8000
+poetry run litestar --app app.main:app database upgrade
+poetry run uvicorn app.main:app --reload --host 0.0.0.0  --port 8000
